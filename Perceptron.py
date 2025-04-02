128a2d606fe4b8e7c113525f6351c7b6fbf74599
Gnani Ramrayani <ramrayanignani@gmail.com>
2025-04-01 21:05:00 -0700

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -1,1 +1,2 @@
-Perceptron proposed by Rosenblatt is a binary classifier in supervised learning. This simple neural network aims to find a hyper plane for a linearly separable data. And it's working is described below.
+#Perceptron proposed by Rosenblatt is a binary classifier in supervised learning. This simple neural network aims to find a hyper plane for a linearly separable data. And it's working is described below.
+from 
